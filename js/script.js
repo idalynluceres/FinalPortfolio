cb200a746bb8e8a0ce7fda5c12a694188fd21581
yayeen <idalynluceres22@gmail.com>
2022-11-01 00:58:14 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// **** Typing Animation **** //
+var typed = new Typed(".typing", {
+    strings:[" ", "Front-End Web Developer.", "Back-End Web Developer.", "Full-Stack Web Developer."],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
